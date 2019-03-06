489ba9c8074daf705c16b25919b379fc7b26f469
miriam1992 <myriiam92@gmail.com>
2019-03-06 19:23:14 +0000

    E4

---
+++ MF0491_3 CLIENTE/UF1841/Examen/js/funcion.js
@@ -0,0 +1,9 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+function square(number) {
+  return number * number;
+}
+
+function 
---
+++ MF0491_3 CLIENTE/UF1841/Examen/js/prueba.js
@@ -0,0 +1,15 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+function pseudocodigo() {
+	var edad;
+	document.write("Digite la edad",'<BR/>');
+	edad = prompt();
+	if (edad>18) {
+		document.write("Ud es mayor de edad",'<BR/>');
+	} else {
+		document.write("Ud es menor de edad",'<BR/>');
+	}
+}
+
